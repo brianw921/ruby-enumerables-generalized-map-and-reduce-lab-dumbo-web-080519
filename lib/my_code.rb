965eb648f676c3e66f7897cc3d813cb6782e9a23
Brian Wong <bwong92@gmail.com>
2019-07-30 17:00:48 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,10 +1,4 @@
 # Your Code Here
 def my_own_map(source_array)
-  new = []
-  i = 0
-  while i < source_array.length do
-    new.push( source_array[i] * -1 ) # <== Unique work
-    i += 1
-  end
-  return new
+  yield(source_array)
 end
