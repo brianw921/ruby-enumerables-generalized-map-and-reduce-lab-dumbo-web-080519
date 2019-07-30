16694ed8c9591151087959e9b360c316d4452a09
Brian Wong <bwong92@gmail.com>
2019-07-30 16:04:52 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,4 +1,10 @@
 # Your Code Here
-def my_own_map(array)
-  array.map { |n| n * 1}
+def map_to_negativize(source_array)
+  new = []
+  i = 0
+  while i < source_array.length do
+    new.push( source_array[i] * -1 ) # <== Unique work
+    i += 1
+  end
+  return new
 end
